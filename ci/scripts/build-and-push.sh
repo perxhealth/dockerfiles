5cf715d361526d51bfb62ed99008279eda542a3a
Luke Rollans <luke@rollans.sh>
2022-09-07 11:19:35 +0800

    First pass at extractions

---
+++ ci/scripts/build-and-push.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Abort if $IMAGE_NAME has not been set
+if [[ -z "$IMAGE_NAME" ]]; then
+  echo "An image name must be provided in the environment" 1>&2
+  exit 1
+fi
+
+# Abort if $IMAGE_VERSION has not been set
+if [[ -z "$IMAGE_VERSION" ]]; then
+  echo "An image version must be provided in the environment" 1>&2
+  exit 1
+fi
+
+# Build, tag and push the image to the authenticated registry
+docker build -t node14-builder:$IMAGE_VERSION -f Dockerfile .
+docker tag node14-builder:$IMAGE_VERSION public.ecr.aws/perx/node14-builder:$IMAGE_VERSION
+docker push public.ecr.aws/perx/node14-builder:$IMAGE_VERSION
