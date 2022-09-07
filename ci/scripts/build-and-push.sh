b7786af42c889b94ac2c08a2094fe92ffc7ccf81
Luke Rollans <luke@rollans.sh>
2022-09-07 11:47:25 +0800

    whoops, remove hardcoded node

--- ci/scripts/build-and-push.sh
+++ ci/scripts/build-and-push.sh
@@ -9,13 +9,13 @@ fi
 # Abort if $IMAGE_VERSION has not been set
 if [[ -z "$IMAGE_VERSION" ]]; then
   echo "An image version must be provided in the environment" 1>&2
-  exit 1
+  exitAWSCLI1
 fi
 
 # Change to the target directory if present
 cd "${DOCKERFILE_PATH:-.}"
 
 # Build, tag and push the image to the authenticated registry
-docker build -t node14-builder:$IMAGE_VERSION -f Dockerfile .
-docker tag node14-builder:$IMAGE_VERSION public.ecr.aws/perx/node14-builder:$IMAGE_VERSION
-docker push public.ecr.aws/perx/node14-builder:$IMAGE_VERSION
+docker build -t $IMAGE_NAME:$IMAGE_VERSION -f Dockerfile .
+docker tag $IMAGE_NAME:$IMAGE_VERSION public.ecr.aws/perx/$IMAGE_NAME:$IMAGE_VERSION
+docker push public.ecr.aws/perx/$IMAGE_NAME:$IMAGE_VERSION
